11b4c9e0187a03735b50c6bd515b6d894be31415
Eimaan Akhtar <eimaanakhtar12@gmail.com>
2025-10-23 20:37:25 -0400

    step 34, created a calculateNextState() method

--- Automaton.java
+++ Automaton.java
@@ -41,6 +41,10 @@ public class Automaton
         System.out.println();
     }   
     
+    private int calculateNextState(int left, int center, int right) {
+     return (left + center + right) % 2;
+    }
+    
     /**
      * Update the automaton to its next state.
      */
@@ -50,12 +54,13 @@ public class Automaton
         int[] nextState = new int[state.length];
         // Naively update the state of each cell
         // based on the state of its two neighbors.
+        int left = 0;
+        int center = state[0];
         for(int i = 0; i < state.length; i++) {
-            int left, center, right;
-            left = (i == 0) ? 0 : state [i - 1];
-            center = state[i];
-            right = (i + 1 < state.length) ? state[i + 1] : 0;
-            nextState[i] = (left + center + right) % 2;
+            int right = i + 1 < state.length ? state[i + 1] : 0;
+            nextState[i] = calculateNextState(left, center, right);
+            left = center;
+            center = right;
         }
         state = nextState;
     }
@@ -71,4 +76,5 @@ public class Automaton
         state[numberOfCells / 2 - 1] = 1;
         state[numberOfCells / 2 + 1] = 1;
     }
+    
 }
